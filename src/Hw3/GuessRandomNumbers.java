25f0368344463bca65a0e29e336d85d8d569a65d
Jackson <kfes867@gmail.com>
2023-09-13 20:52:51 +0800

    HomeWork3 add advance

--- src/Hw3/GuessRandomNumbers.java
+++ src/Hw3/GuessRandomNumbers.java
@@ -5,18 +5,44 @@ import java.util.Scanner;
 
 public class GuessRandomNumbers {
     public static void main(String[] args) {
+        GuessRandomNumbers guessRandomNumbers = new GuessRandomNumbers();
         Random random = new Random();
-        Scanner input = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("猜數字:");
-        int guessNumber = input.nextInt();
-        int randomNumber = random.nextInt(10);
+        int guessNumber = scanner.nextInt();
+
+        int randomNumber = random.nextInt(101);//產生0到100的亂數
+        guessRandomNumbers.guessNum(guessNumber, randomNumber);//傳入猜測數字和已產生的目標亂數，使用"物件"調用方法
+    }
+    public void guessNum(int guessNumber, int randomNumber){
+        Scanner scanner = new Scanner(System.in);
 
         while (guessNumber != randomNumber){
+
+            if(guessNumber > randomNumber){
+                System.out.println("猜測數字大於正確答案");
+            }else {
+                System.out.println("猜測數字小於正確答案");
+            }
             System.out.println("猜錯了，再猜一次吧");
-            guessNumber = input.nextInt();
+            guessNumber = scanner.nextInt();
         }
         System.out.println("答對了，答案是:" + randomNumber);
-        input.close();
     }
+//    public static void main(String[] args) {
+//        Random random = new Random();
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.println("猜數字:");
+//        int guessNumber = input.nextInt();
+//        int randomNumber = random.nextInt(10);
+//
+//        while (guessNumber != randomNumber){
+//            System.out.println("猜錯了，再猜一次吧");
+//            guessNumber = input.nextInt();
+//        }
+//        System.out.println("答對了，答案是:" + randomNumber);
+//        input.close();
+//    }
 }
--- src/Hw3/Triangle.java
+++ src/Hw3/Triangle.java
@@ -4,13 +4,19 @@ import java.util.Scanner;
 
 public class Triangle {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+
+        Triangle triangle = new Triangle();
+        Scanner scanner = new Scanner(System.in);
 
         int x, y, z;
         System.out.println("請輸入三角形的三個邊:");
-        x = input.nextInt();
-        y = input.nextInt();
-        z = input.nextInt();
+        x = scanner.nextInt();
+        y = scanner.nextInt();
+        z = scanner.nextInt();
+
+        triangle.judgeTriangle(x, y, z);
+    }
+    public void judgeTriangle(int x, int y, int z){
 
         if (x + y > z && y + z > x && x + z > y) {
             if (x == y && y == z) {
@@ -23,6 +29,5 @@ public class Triangle {
         } else {
             System.out.println("這不是三角形");
         }
-        input.close();
     }
 }
